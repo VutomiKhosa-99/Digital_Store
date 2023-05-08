90bc7301f01a81d87b6c5b46710e40472c15f0b0
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-05-08 16:33:14 +0200

    User routes and controllers created

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,33 +1,50 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    
-    firstName: {
-        type: String,
-        required: true,
-        min: 2,
-        max: 50,
-      },
-      lastName: {
-        type: String,
-        required: true,
-        min: 2,
-        max: 50,
-      },
+
     username: {
         type: String,
         required: true
     },
-    email: {
-        type: String,
-        required: true,
-        max: 50,
-        unique: true,
-      },
     password: {
         type: String,
         required: true
     },
+    roles: {
+        type: [String],
+        default: ["Employee"]
+    }
+    
+    // firstName: {
+    //     type: String,
+    //     required: true,
+    //     min: 2,
+    //     max: 50,
+    //   },
+    //   lastName: {
+    //     type: String,
+    //     required: true,
+    //     min: 2,
+    //     max: 50,
+    //   },
+    // username: {
+    //     type: String,
+    //     required: true
+    // },
+    // email: {
+    //     type: String,
+    //     required: true,
+    //     max: 50,
+    //     unique: true,
+    //   },
+    // password: {
+    //     type: String,
+    //     required: true
+    // },
+    // roles: {
+    //     type: [String],
+    //     default: ["Employee"]
+    // },
 },
     { timestamps: true }
 )
