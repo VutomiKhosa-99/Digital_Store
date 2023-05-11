7a025c8333a04552cfc0f99647c02966afa69600
Altharia <althariam@gmail.com>
2023-05-11 14:13:08 +0200

    Add frontend

---
+++ frontend/client/src/app/iProduct.ts
@@ -0,0 +1,12 @@
+export interface IProduct {
+    title:string;
+    description:string;
+    size:string;
+    color:string;
+    price:string;
+    category:string;
+    brand:string;
+    image:string;
+    availableStock:number;
+
+}
