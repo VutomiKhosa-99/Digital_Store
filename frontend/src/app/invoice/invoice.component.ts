46d3f886edb37111c8b8dce38cad61be518e7308
Prince-Kay99 <ph.kubaye@gmail.com>
2023-05-25 09:52:50 +0200

    Added invoice

--- frontend/src/app/invoice/invoice.component.ts
+++ frontend/src/app/invoice/invoice.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.css']
 })
-export class InvoiceComponent {
+export class InvoiceComponent implements OnInit{
+
+
+  constructor(public cartService: CartService){
+
+  }
+
+  ngOnInit(): void {}
 
 }
