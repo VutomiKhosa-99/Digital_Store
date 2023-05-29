8fa99f0e655259ebcbad592c26655bf433d0b1fc
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-05-29 16:31:04 +0200

    Added frontend link

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:4200',
     'http://localhost:6000',
-    'https://digital-store-api.onrender.com'
+    'https://digital-store-api.onrender.com',
+    'https://digitalfs.netlify.app'
     
 ]
 
