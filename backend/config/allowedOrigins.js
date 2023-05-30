10eb6d49d198da1914c8fd1486fc0c0b13c2f42b
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-05-30 09:18:06 +0200

    Added our frontend link to be allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:4200',
     'http://localhost:6000',
     'https://digital-store-api.onrender.com',
-    'https://digitalfs.netlify.app'
+    'https://dfs-six.vercel.app'
+   
     
 ]
 
